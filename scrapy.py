ac11afda38011bcfca7fad9d7aa4c1a47b4e8f6d
jarinbanji-dotcom <jarinbanji@gmail.com>
2026-02-19 11:59:26 +0530

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -10,8 +10,9 @@ session.headers.update({
 
 response = session.get(
     "https://api-manager.upbit.com/api/v1/announcements",
-    params={"os":"web","page":1,"per_page":20,"category":"all"}
+    params={"os":"web","page":1,"per_page":20,"category":"trade"}
 )
 
 print(response.json())
+
 
