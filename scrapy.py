296774e2850c7404272080e3abdffd5c8a759ae0
jarinbanji-dotcom <jarinbanji@gmail.com>
2026-02-19 12:01:17 +0530

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -1,18 +1,25 @@
 import requests
 
-session = requests.Session()
+url = "https://api-manager.upbit.com/api/v1/announcements"
 
-session.headers.update({
+headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
     "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Origin": "https://upbit.com",
     "Referer": "https://upbit.com/"
-})
+}
 
-response = session.get(
-    "https://api-manager.upbit.com/api/v1/announcements",
-    params={"os":"web","page":1,"per_page":20,"category":"trade"}
-)
+params = {
+    "os": "web",
+    "page": 1,
+    "per_page": 10,
+    "category": "all"
+}
 
-print(response.json())
+res = requests.get(url, headers=headers, params=params, timeout=30)
+print(res)
+
+
 
 
